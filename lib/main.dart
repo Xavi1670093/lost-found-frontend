d40c195f327969b477fde3f9bfe6c06267b033bc
Xavi <xavigra2004@gmail.com>
2026-04-06 12:24:04 +0200

    it2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:unilost_found/app.dart';
 import 'package:unilost_found/core/settings/app_settings_controller.dart';
 
